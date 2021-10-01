04f87d887882c99ad30fc922cc9b1804b94d18f9
Heimlich <jerry.lin@tradevan.com.tw>
2021-10-01 15:12:50 +0800

    to git

---
+++ src/main/java/idv/heimlich/base/App.java
@@ -0,0 +1,13 @@
+package idv.heimlich.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
